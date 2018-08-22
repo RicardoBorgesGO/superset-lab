ea978f16bdb510ad244a5131322ff1af06e6eab6
dannyelcf <dannyelcf@gmail.com>
2018-08-21 21:58:48 -0300

    Changed Dockerfile to execute superset on debug mode. Change superset-demo to not show prompt in the admin user creation. Bugfix in examples/sqlite/customs/analisa/security.py

--- Dockerfile
+++ Dockerfile
@@ -66,5 +66,5 @@ WORKDIR /home/superset
 # Deploy application
 EXPOSE 8088
 HEALTHCHECK CMD ["curl", "-f", "http://localhost:8088/health"]
-CMD ["gunicorn", "-w", "2", "--timeout", "60", "-b", "0.0.0.0:8088", "--limit-request-line", "0", "--limit-request-field_size", "0", "superset:app"]
+CMD ["superset", "runserver", "-d"]
 USER superset
--- examples/sqlite/customs/analisa/security.py
+++ examples/sqlite/customs/analisa/security.py
@@ -5,10 +5,19 @@ from flask_appbuilder.security.views import expose
 from flask_appbuilder.security.manager import BaseSecurityManager
 from flask_login import login_user, logout_user
 
+LOGIN_PAGE_CONTENT = """
+    <html>
+        <body>
+        <a href="http://localhost:8088/login?username=admin&redirect=/superset/welcome">This link will auto login as admin</a>
+        <body>
+    <html>
+"""
+
 # Based on https://medium.com/@sairamkrish/apache-superset-custom-authentication-and-integrate-with-other-micro-services-8217956273c1
 class CustomAuthDBView(AuthDBView):
     @expose('/login/', methods=['GET', 'POST'])
     def login(self):
+
         redirect_url = self.appbuilder.get_url_for_index
         if request.args.get('redirect') is not None:
             redirect_url = request.args.get('redirect')
@@ -18,18 +27,16 @@ class CustomAuthDBView(AuthDBView):
 
         if request.args.get('username') is not None:
             user = self.appbuilder.sm.find_user(username=request.args.get('username'))
-            login_user(user, remember=False)
-            return redirect(redirect_url)
+            if user is not None:
+                login_user(user, remember=False)
+                return redirect(redirect_url)
+            else:
+                return "Unknow user '{}'".format(request.args.get('username'))
+
         elif g.user is not None and g.user.is_authenticated():
             return redirect(redirect_url)
         else:
-            return """
-                <html>
-                  <body>
-                    <a href="http://localhost:8088/login?username=admin&redirect=/superset/welcome">This link will auto login as admin</a>
-                  <body>
-                <html>
-            """
+            return LOGIN_PAGE_CONTENT
 
 class CustomSecurityManager(SupersetSecurityManager):
     authdbview = CustomAuthDBView
